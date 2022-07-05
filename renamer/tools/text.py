0d81b747e8f7407f16f6950dd3bbed460c558599
supersip <108521768+supersip@users.noreply.github.com>
2022-07-05 15:48:42 +0530

    Update text.py

--- renamer/tools/text.py
+++ renamer/tools/text.py
@@ -3,7 +3,7 @@ from pyrogram.emoji import *
 class TEXT:
     DOWNLOAD_START = f"Downloading Starts soon {SLEEPING_FACE}"
     UPLOAD_START = f"Upload Starting Soon {SLEEPING_FACE}"
-    UPLOAD_SUCESS = f"Thanks for using [me](https://github.com/Ns-AnoNymouS/TG-RENAMER-BOT)"
+    UPLOAD_SUCESS = f"Thanks for using [me](https://t.me/XDecoy)"
     BANNED_USER_TEXT = f"Hey bro, you are **banned** from using me {FACE_WITH_TEARS_OF_JOY}."
     NOT_LOGGED_TEXT = f"This bot was only for private use {LOCKED_WITH_KEY}. If you want to use this bot you need to send me correct password in the format `/login bot_password`"
     SAVED_CUSTOM_THUMBNAIL = f"Thumbnail Saved Permanently {NOTEBOOK_WITH_DECORATIVE_COVER}"
@@ -22,11 +22,11 @@ class TEXT:
 
 ** Developer:** {bot_owner}
 
-** Channel:** [NS BOT UPDATES](https://t.me/Ns_bot_updates)
+** Channel:** [NS BOT UPDATES](https://t.me/RestinHeaven)
 
-** Group:** [Ns BOT SUPPORT](https://t.me/Ns_Bot_supporters)
+** Group:** [Ns BOT SUPPORT](https://t.me/RestinHeaven)
 
-** Source Code:** [Press Me](https://github.com/Ns-AnoNymouS/TG-RENAMER-BOT)
+** Source Code:** [Press Me](https://t.me/HeavenBotSupport)
 """
 
     HELP_USER = """**Follow Below Steps:**
@@ -35,8 +35,7 @@ class TEXT:
 ☞︎︎︎ Send a photo to set as permanent thumbnail.
 ☞︎︎︎ Now send me the Telegram file you want to rename.
 ☞︎︎︎ Send the new name when bot ask.
-
-For source code check about
+for Any issue @HeavenBotSupport
 """
 
     START_TEXT = """Hi {user_mention},
@@ -51,12 +50,4 @@ I am a telegram renamer with permanent thumbnail.
 
     DONATE_USER = """**__Thanks for showing interest in donation.__**
  
-Donate us to keep our services continously alive
-You can send any amount 
-of 20rs, 30rs, 50rs, 70rs, 100rs, 200rs
- 
-__--Payment Methods:--__
- 
-GooglePay / Paytm / PhonPay / paypal / Net Banking
- 
-**For Donate:** message me @MaxxRiderz"""
+sorry no thanks"""
